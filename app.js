5d70db2dea59064ac418a3b07382c444351a6cf0
Koceryba <koceryba.s@gmail.com>
2018-01-18 10:14:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         arr: ''
+//     },
+//     methods: {
+//         go: 
+        // function () {
+            var arr = '2,4,3,6,7'
+            var res;
+            var arrTrue = arr.split(',');
+            var arrStep = [];
+
+            console.log(arrTrue)
+
+            for ( var i = 0; i < arrTrue.length; i++ ) {
+                console.log(i)
+                if ( arrTrue[i] % 2 == 0) {
+                    res = -1;
+                } else {
+                    res = true;
+                    break
+                }
+            }
+
+            function gcd(n, m) {
+                if(m > 0) { 
+                    var k = n%m;
+                    return gcd(m, k); 
+                } 
+                else { 
+                    return Math.abs(n);  
+                }
+            }
+
+            for ( var j = 0; j < arrTrue.length-1       ; j++ ) {
+                arrStep.push(gcd(arrTrue[j], arrTrue[j+1]))
+            }
+
+            console.log(gcd(347, 5884))
+            console.log(gcd(5884, 833))
+        // }
+//     }
+//   })
