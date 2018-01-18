64ea81b29b61e676ea38538fec8629c54d8910ff
Koceryba <koceryba.s@gmail.com>
2018-01-18 11:26:44 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,33 +1,39 @@
 
-            var arr = '2,4,3,6,7'
-            var res;
-            var arrTrue = arr.split(',');
-            var arrStep = [];
+/*
+Если в gcd есть 1, кол-во шагов равно, величине массива.
+Если мы можем привести gcd к 1, кол-во шагов равно, величина массива + кол-во шагов к gcd = 1. 
+Если все элементы массива кратны 2, нельзя.
+Если все gcd кратны одному числу (>1) нельзя.
+Задачу не дописывал, не уверен в логике.
+*/
 
-            console.log(arrTrue)
+var arr = '2,4,3,6,7'
+var res;
+var arrTrue = arr.split(',');
+var arrStep = [];
 
-            for ( var i = 0; i < arrTrue.length; i++ ) {
-                console.log(i)
-                if ( arrTrue[i] % 2 == 0) {
-                    res = -1;
-                } else {
-                    res = true;
-                    break
-                }
-            }
+console.log(arrTrue)
 
-            function gcd(n, m) {
-                if(m > 0) { 
-                    var k = n%m;
-                    return gcd(m, k); 
-                } 
-                else { 
-                    return Math.abs(n);  
-                }
-            }
+for ( var i = 0; i < arrTrue.length; i++ ) {
+    console.log(i)
+    if ( arrTrue[i] % 2 == 0) {
+        res = -1;
+    } else {
+        res = true;
+        break
+    }
+}
 
-            for ( var j = 0; j < arrTrue.length-1       ; j++ ) {
-                arrStep.push(gcd(arrTrue[j], arrTrue[j+1]))
-            }
+function gcd(n, m) {
+    if(m > 0) { 
+        var k = n%m;
+        return gcd(m, k); 
+    } 
+    else { 
+        return Math.abs(n);  
+    }
+}
 
-           
+for ( var j = 0; j < arrTrue.length-1       ; j++ ) {
+    arrStep.push(gcd(arrTrue[j], arrTrue[j+1]))
+}
